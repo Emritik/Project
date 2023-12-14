6f7a82fa6e4c10dd6c48f14ce7d6db905c9e71b3
Ritik sharma <98308171+Emritik@users.noreply.github.com>
2023-12-15 01:02:06 +0530

    Update settings.py

--- Project/settings.py
+++ Project/settings.py
@@ -146,5 +146,4 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "STETinfoexam@gmail.com"
-EMAIL_HOST_PASSWORD = "ecwh bnsj uunh cbuh"
-# password = ecwh bnsj uunh cbuh
+EMAIL_HOST_PASSWORD = "######3"
